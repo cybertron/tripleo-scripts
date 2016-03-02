a225156c562defffd81cf61cc3b190ad07b51647
Ben Nemec <bnemec@redhat.com>
2016-03-01 18:27:23 -0600

    Add some basic sanity tests
    These tests verify that the output generated by the various methods
    in net_processing doesn't change, or at least ensures that if it
    does change the tester is aware of it.

---
+++ test-net-iso-gen.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# Copyright 2016 Red Hat Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+import os
+import shutil
+import tempfile
+import unittest
+
+import net_processing
+
+class TestOutput(unittest.TestCase):
+    def tearDown(self):
+        shutil.rmtree(self.output_path)
+
+    def _test(self, input_path):
+        self.output_path = tempfile.mkdtemp()
+        with open(os.path.join(input_path, 'nic-input.json')) as f:
+            data = json.loads(f.read())
+        with open(os.path.join(input_path, 'global-input.json')) as f:
+            global_data = json.loads(f.read())
+        net_processing._write_net_env(data, global_data, self.output_path)
+        with open(os.path.join(self.output_path,
+                               'network-environment.yaml')) as actual:
+            with open(os.path.join(input_path,
+                                   'network-environment.yaml')) as expected:
+                self.assertEqual(expected.read(), actual.read())
+
+        net_processing._write_net_iso(data, self.output_path)
+        with open(os.path.join(self.output_path,
+                               'network-isolation.yaml')) as actual:
+            with open(os.path.join(input_path,
+                                   'network-isolation.yaml')) as expected:
+                self.assertEqual(expected.read(), actual.read())
+
+        net_processing._write_nic_configs(data, self.output_path)
+        opath = os.path.join(self.output_path, 'nic-configs')
+        for f in os.listdir(opath):
+            with open(os.path.join(opath, f)) as actual:
+                with open(os.path.join(input_path,
+                                       'nic-configs', f)) as expected:
+                    self.assertEqual(expected.read(), actual.read())
+
+    def test_nics_basic(self):
+        self._test('test-data/nics-basic')
+
+    def test_all_the_things(self):
+        self._test('test-data/all-the-things')
+
+
+if __name__ == '__main__':
+    unittest.main()
